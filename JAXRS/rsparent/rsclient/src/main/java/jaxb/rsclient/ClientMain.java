3d6fc19e933a0c433119bcfcd5721b1d8c4abcef
danecek <danecek@fit.cvut.cz>
2015-04-28 09:32:00 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- JAXRS/rsparent/rsclient/src/main/java/jaxb/rsclient/ClientMain.java
+++ JAXRS/rsparent/rsclient/src/main/java/jaxb/rsclient/ClientMain.java
@@ -6,13 +6,16 @@
 package jaxb.rsclient;
 
 //import com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import jaxb.rsservice.MyBean;
+import jaxb.rsservice.MyReaderInterceptor;
 import org.glassfish.jersey.filter.LoggingFilter;
 
 /**
@@ -27,18 +30,22 @@ public class ClientMain {
 //        //    com.sun.xml.ws.transport.http.client
 
         Client client = ClientBuilder.newClient();
-  
-        client.register(new LoggingFilter(Logger.getAnonymousLogger(), true));
-        //client.
-      //  client.register(new MyInterceptor());
-        WebTarget target = client.target("https://localhost:4463").path("resource");
+
+        client.register(new MyLoggingFilter());
+        client.register(new MyReaderInterceptor());
+        WebTarget target = client.target("http://localhost:9998").path("resource");
+
 //        Form form = new Form();
 //        form.param("x", "foo");
 //        form.param("y", "bar");
         //  MyJAXBBean bean
         Response result = target.request(MediaType.APPLICATION_JSON)
                 .get();
-        System.out.println(result.readEntity(MyBean.class));
+        // System.out.println(result.getEntity()); 
+
+        List<MyBean> beanList = result.readEntity(new GenericType<List<MyBean>>() {
+        });
+        System.out.println(beanList);
     }
 
 }
